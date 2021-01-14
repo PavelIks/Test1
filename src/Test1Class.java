6290e08ca128669e00307191deb7bb55bc16c7a9
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-01-14 18:16:30 +0200

    Updated-4.

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -6,44 +6,108 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*Простой пример*/
+/*@WebServlet("/Test1Class")
+public class Test1Class extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h2>Hello Web Java!</h2>");
+		writer.println("<h2 style='color:red'>Hello Web Java!</h2>");
+	}
+}*/
+
+/*Применение doGet*/
+/*@WebServlet("/Test1Class")
+public class Test1Class extends HttpServlet
+{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter writer1 = response.getWriter();
+	
+		writer1.println("<h2 style='color:green'>Hello Web Java!</h2>");
+		if(request.getParameter("id") != null)
+		{
+			writer1.println("<h2>You entered: " + request.getParameter("id") + "</h2>");
+		}
+		else
+		{
+			try
+			{
+				writer1.println("<h2>ID not entered yet\nEnter for example ?id=11</h2>");
+			}
+			catch(Exception exception1)
+			{
+				writer1.println("Error: " + exception1);
+			}
+			finally
+			{
+				writer1.close();
+			}
+		}
+	}
+}*/
+
+/**/
 @WebServlet("/Test1Class")
 public class Test1Class extends HttpServlet
 {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		
-		// 1 param
-//		String text = "<h2>Hello Web Java!</h2>";
-//		if(request.getParameter("id") != null) {
-//			text = "<h1>User id is " + request.getParameter("id") + "</h1>";
-//		}
-		
-//		try {
-//			writer.println(text);
-//			writer.println("<h2 style='color:red'>Hello Web Java!</h2>");
-			
-//			writer.println("Id of user is " + param1);
-//
-//		}
-//		
-//		finally {
-//			writer.close();
-//		}
-		
-		//array
+		PrintWriter writer1 = response.getWriter();
 		String[] nums = request.getParameterValues("nums");
-		try
-		{
-			writer.println("<h1>Numbers from link</h1>");
-			for(String n: nums)
+		writer1.println("<h2 style='color:green'>Hello Web Java!</h2>");
+//		if(request.getParameter("id") != null)
+//		{
+//			writer1.println("<h2>You entered: " + request.getParameter("id") + "</h2>");
+//		}
+//		else
+//		{
+			try
 			{
-				writer.println(n + ",");
+				writer1.println("<h2>ID not entered yet\nEnter for example ?nums=1&nums=2&nums=3</h2>");
+				for(String n: nums)
+				{
+					writer1.print(n + "");
+				}
 			}
-		}
-		finally
-		{
-			writer.close();
-		}
+//			catch(Exception exception1)
+//			{
+//				writer1.println("Error: " + exception1);
+//			}
+			finally
+			{
+				writer1.close();
+			}
+		//}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
