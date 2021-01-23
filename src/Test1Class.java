2877ac40efadd45175f699b36ee930976296b26a
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-01-24 01:45:27 +0200

    Updated-5.

--- src/Test1Class.java
+++ src/Test1Class.java
@@ -51,7 +51,7 @@ public class Test1Class extends HttpServlet
 	}
 }*/
 
-/**/
+/*Применение doGet: String-массив*/
 @WebServlet("/Test1Class")
 public class Test1Class extends HttpServlet
 {
@@ -59,14 +59,15 @@ public class Test1Class extends HttpServlet
 	{
 		response.setContentType("text/html");
 		PrintWriter writer1 = response.getWriter();
+		
 		String[] nums = request.getParameterValues("nums");
 		writer1.println("<h2 style='color:green'>Hello Web Java!</h2>");
-//		if(request.getParameter("id") != null)
-//		{
-//			writer1.println("<h2>You entered: " + request.getParameter("id") + "</h2>");
-//		}
-//		else
-//		{
+		if(request.getParameter("id") != null)
+		{
+			writer1.println("<h2>You entered: " + request.getParameter("id") + "</h2>");
+		}
+		else
+		{
 			try
 			{
 				writer1.println("<h2>ID not entered yet\nEnter for example ?nums=1&nums=2&nums=3</h2>");
@@ -75,39 +76,10 @@ public class Test1Class extends HttpServlet
 					writer1.print(n + "");
 				}
 			}
-//			catch(Exception exception1)
-//			{
-//				writer1.println("Error: " + exception1);
-//			}
 			finally
 			{
 				writer1.close();
 			}
-		//}
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
